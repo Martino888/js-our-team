c193a3cb02bdba42093165ca89798c5f5746d44a
Martino888 <maxmartino33@gmail.com>
2022-03-07 17:46:28 +0100

    obj-card

--- script.js
+++ script.js
@@ -2,3 +2,37 @@
 // Prendendo come riferimento la card di esempio presente nell'html, stampiamo dinamicamente una card per ogni membro del team.
 // BONUS:
 // Utilizziamo gli input presenti nella pagina per permettere all'utente di aggiungere nuovi membri del team: cliccando sul pulsante "add" viene creato un nuovo oggetto, il quale viene inserito nell'array iniziale, e viene stampata una nuova card con tutte le informazioni inserite dall'utente.
+
+
+
+let card = [
+    {
+    name: 'Wayne Barnett',
+    role: 'Founder & CEO',
+    img:  'img/wayne-barnett-founder-ceo.jpg',
+}
+{
+    name: 'Angela Caroll',
+    role: 'Chief Editor',
+    img:  'img/angela-caroll-chief-editor.jpg',
+}
+{
+    name: 'Walter Gordon',
+    role: 'Office Manager',
+    img:  'img/walter-gordon-office-manager.jpg',
+}
+{
+    name: 'Angela Lopez',
+    role: 'Social Media Manager',
+    img:  'img/angela-lopez-social-media-manager.jpg',
+}
+{
+    name: 'Scott Estrada',
+    role: 'Developer',
+    img:  'img/scott-estrada-developer.jpg',
+}
+{
+    name: 'Barbara Ramos',
+    role: 'Graphic Designer',
+    img:  'img/barbara-ramos-graphic-designer.jpg',
+}]
