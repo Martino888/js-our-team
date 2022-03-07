1d3ac74416090b52e87b2899b5eaf5775b5bc0f1
Martino888 <maxmartino33@gmail.com>
2022-03-07 19:22:24 +0100

    con bonus

--- script.js
+++ script.js
@@ -5,34 +5,67 @@
 
 
 
-let card = [
+let card = [{
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        img: 'img/wayne-barnett-founder-ceo.jpg',
+    },
     {
-    name: 'Wayne Barnett',
-    role: 'Founder & CEO',
-    img:  'img/wayne-barnett-founder-ceo.jpg',
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        img: 'img/angela-caroll-chief-editor.jpg',
+    },
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        img: 'img/walter-gordon-office-manager.jpg',
+    },
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        img: 'img/angela-lopez-social-media-manager.jpg',
+    },
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        img: 'img/scott-estrada-developer.jpg',
+    },
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        img: 'img/barbara-ramos-graphic-designer.jpg',
+    }
+]
+
+const btn = document.getElementById('addMemberButton');
+aggiornaMembri();
+function aggiornaMembri(){
+    let contenuto = '';
+for (let i = 0; i < card.length; i++) {
+    contenuto += `<div class="team-card">
+                <div class="card-image">
+                <img
+                    src="${card[i].img}"
+                    alt="${card[i].name}"/>
+                </div>
+                <div class="card-text">
+                <h3>${card[i].name}</h3>
+                <p>${card[i].role}</p>
+                </div>
+                </div>
+                </div>`;
 }
-{
-    name: 'Angela Caroll',
-    role: 'Chief Editor',
-    img:  'img/angela-caroll-chief-editor.jpg',
+document.querySelector('.team-container').innerHTML = contenuto;
 }
-{
-    name: 'Walter Gordon',
-    role: 'Office Manager',
-    img:  'img/walter-gordon-office-manager.jpg',
+
+btn.addEventListener('click',aggiuntaCard)
+
+function aggiuntaCard() {
+    let nomeInput = document.querySelector('#name').value;
+    let roleInput = document.querySelector('#role').value;
+    let imageInput = document.querySelector('#image').value;
+
+    card.push({name: nomeInput, role: roleInput, img: imageInput});
+    aggiornaMembri();
+    
 }
-{
-    name: 'Angela Lopez',
-    role: 'Social Media Manager',
-    img:  'img/angela-lopez-social-media-manager.jpg',
-}
-{
-    name: 'Scott Estrada',
-    role: 'Developer',
-    img:  'img/scott-estrada-developer.jpg',
-}
-{
-    name: 'Barbara Ramos',
-    role: 'Graphic Designer',
-    img:  'img/barbara-ramos-graphic-designer.jpg',
-}]
